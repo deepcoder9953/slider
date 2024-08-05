cb5d89a7fef477275568f22b51063963075a8921
deepcoder9953 <165238037+deepcoder9953@users.noreply.github.com>
2024-08-05 10:11:22 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,32 @@
+ let index = 0;
+ let lol = 156;
+slideFun(index)
+
+
+function onSlide(num){
+    index = index + num
+    slideFun(index)
+}
+
+function slideFun(idx){
+    var alldivs = document.querySelectorAll(".slides")
+
+
+    if(idx<=0){
+        index = alldivs.length
+    }
+    
+    else if(idx>alldivs.length){
+        index = 1
+    }
+
+    for(var i=0; i<alldivs.length; i++){
+        alldivs[i].style.display = "none"
+    }
+
+   if (alldivs[index - 1]) {
+    alldivs[index - 1].style.display = "block";
+} else {
+    console.error("Slide not found for index:", index);
+}
+}
